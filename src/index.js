7d616c09e830cb3803cc5a706f16cb7d62a52bd4
ayushanand89 <ayushanand0108@gmail.com>
2025-05-05 03:29:08 +0530

    add user controller & route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 //require('dotenv').config({ path: './env'})
 import dotenv from "dotenv";
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+  path: "./env",
+});
+
+
+
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
